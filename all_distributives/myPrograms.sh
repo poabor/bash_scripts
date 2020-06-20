9891974c6ad17ddd9e25bec49c5701d2380df354
poabor <poabor@gmail.com>
2020-06-20 09:12:21 +0300

    resilio-sync обновление

--- all_distributives/myPrograms.sh
+++ all_distributives/myPrograms.sh
@@ -4,9 +4,11 @@ sudo apt-get install gramps -y
 echo '> gramps установлен!'
 
 # Скрип устанавливает/обновляет resilio-sync
+sudo apt install curl -y
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
 curl -LO http://linux-packages.resilio.com/resilio-sync/key.asc && sudo apt-key add ./key.asc
 sudo apt-get update
-sudo apt-get install resilio-sync
+sudo apt-get install resilio-sync -y
 sudo systemctl enable resilio-sync
+sudo service resilio-sync start
 echo '> resilio-sync установлен!'
